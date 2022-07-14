8b89d01def067a11f5013f44e18b7eab31335442
Alex Eden <alexandereden91@gmail.com>
2022-07-13 23:28:49 -0500

    Moar boilerplate

--- examples/rotary_encoder_test.rs
+++ examples/rotary_encoder_test.rs
@@ -1,12 +1,29 @@
 #![no_std]
 #![no_main]
+use adafruit_seesaw::Seesaw;
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal::{
+    gpio::GpioExt,
+    i2c::I2c,
+    pac,
+    prelude::*,
+    rcc::{RccExt, SYSCLK_MAX},
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("Starting");
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+    let gpiob = dp.GPIOB.split();
+    let clocks = dp.RCC.constrain().cfgr.sysclk(SYSCLK_MAX.Hz()).freeze();
+    let delay = cp.SYST.delay(&clocks);
+    let scl = gpiob.pb6.into_alternate_open_drain::<4>();
+    let sda = gpiob.pb7.into_alternate_open_drain::<4>();
+    let i2c = I2c::new(dp.I2C1, (scl, sda), 100.kHz(), &clocks);
+    let _ss = Seesaw::new(i2c, delay);
 
     loop {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,29 @@
 #![no_std]
-pub const PLACEHOLDER: &str = "PLACEHOLER";
+use embedded_hal::blocking::{
+    delay::DelayUs,
+    i2c::{Write, WriteRead},
+};
+
+pub struct Seesaw<I2C, DELAY> {
+    bus: I2C,
+    delay: DELAY,
+}
+
+impl<I2C, DELAY> DelayUs<u32> for Seesaw<I2C, DELAY>
+where
+    DELAY: DelayUs<u32>,
+{
+    fn delay_us(&mut self, us: u32) {
+        self.delay.delay_us(us)
+    }
+}
+
+impl<I2C, DELAY, E> Seesaw<I2C, DELAY>
+where
+    DELAY: DelayUs<u32>,
+    I2C: WriteRead<Error = E> + Write<Error = E>,
+{
+    pub fn new(bus: I2C, delay: DELAY) -> Self {
+        Seesaw { bus, delay }
+    }
+}
