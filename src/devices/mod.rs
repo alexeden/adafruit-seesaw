03ac5cbf07c4c5d9c2df66005dafe82cae05a914
Alex Eden <alexandereden91@gmail.com>
2022-11-28 04:40:57 -0600

    Put generic device back into its own mod

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -1,7 +1,6 @@
 use embedded_hal::blocking::{delay, i2c};
-// mod generic_device;
-// pub use generic_device::*;
-// use shared_bus::BusMutex;
+mod generic_device;
+pub use generic_device::*;
 
 pub trait Device<D: i2c::Write + i2c::WriteRead + i2c::Read + delay::DelayUs<u32>> {
     fn addr(&self) -> u8;
@@ -11,12 +10,13 @@ pub trait Device<D: i2c::Write + i2c::WriteRead + i2c::Read + delay::DelayUs<u32
     fn driver<'a>(&'a mut self) -> &'a mut D;
 }
 
+#[macro_export]
 macro_rules! seesaw_device {
     ($device:ident, $( $x:ty ),*) => {
         #[derive(Debug)]
         pub struct $device<M>(u8, M);
 
-        impl<D: crate::Driver> Device<D> for $device<D> {
+        impl<D: crate::Driver> $crate::Device<D> for $device<D> {
             fn addr(&self) -> u8 {
                 self.0
             }
@@ -32,20 +32,9 @@ macro_rules! seesaw_device {
     };
 }
 
-seesaw_device!(GenericDevice, StatusModule);
-
 impl<T, D> crate::StatusModule<D> for T
 where
     D: crate::Driver,
     T: Device<D>,
 {
 }
-
-// impl<D: Driver> GenericDevice<D> {
-// impl<D: i2c::Write + i2c::WriteRead + i2c::Read + delay::DelayUs<u32>>
-// GenericDevice<D> {     // GenericDevice<D> {
-
-//     pub fn new(addr: u8, driver: D) -> Self {
-//         Self(addr, driver)
-//     }
-// }
