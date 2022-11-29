99b7bbd1adf9b783ebe8b965a99c3215f02bf782
Alex Eden <alexandereden91@gmail.com>
2022-11-29 02:15:15 -0600

    Restore the (broken) neoslider example

--- examples/neoslider_test.rs
+++ examples/neoslider_test.rs
@@ -1,11 +1,29 @@
 #![no_std]
 #![no_main]
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+use adafruit_seesaw::{devices::NeoSlider, SeesawSingleThread};
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal::{gpio::GpioExt, i2c::I2c, pac, prelude::*, rcc::RccExt};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+    let gpiob = dp.GPIOB.split();
+    let clocks = dp.RCC.constrain().cfgr.freeze();
+    let delay = cp.SYST.delay(&clocks);
+    let scl = gpiob.pb6.into_alternate_open_drain::<4>();
+    let sda = gpiob.pb7.into_alternate_open_drain::<4>();
+    let i2c = I2c::new(dp.I2C1, (scl, sda), 100.kHz(), &clocks);
+    let seesaw = SeesawSingleThread::new(delay, i2c);
+    let mut _neoslider = seesaw
+        .connect_default_addr::<NeoSlider<_>>()
+        .expect("Failed to connect");
+
+    // It never gets this far...
     loop {}
 }
 
@@ -18,54 +36,3 @@ fn handle_panic(info: &core::panic::PanicInfo) -> ! {
     }
     loop {}
 }
-
-// #![no_std]
-// #![no_main]
-// use adafruit_seesaw::{
-//     devices::{neoslider::NeoSlider, SeesawDevice},
-//     modules::status::StatusModule,
-//     SeesawBus,
-// };
-// use cortex_m_rt::entry;
-// use rtt_target::{rprintln, rtt_init_print};
-// use stm32f4xx_hal::{
-//     gpio::GpioExt,
-//     i2c::I2c,
-//     pac,
-//     prelude::*,
-//     rcc::{RccExt, SYSCLK_MAX},
-// };
-
-// #[entry]
-// fn main() -> ! {
-//     rtt_init_print!();
-//     rprintln!("Starting");
-//     let cp = cortex_m::Peripherals::take().unwrap();
-//     let dp = pac::Peripherals::take().unwrap();
-//     let gpiob = dp.GPIOB.split();
-//     let clocks = dp.RCC.constrain().cfgr.sysclk(SYSCLK_MAX.Hz()).freeze();
-//     let delay = cp.SYST.delay(&clocks);
-//     let scl = gpiob.pb6.into_alternate_open_drain::<4>();
-//     let sda = gpiob.pb7.into_alternate_open_drain::<4>();
-//     let i2c = I2c::new(dp.I2C1, (scl, sda), 400.kHz(), &clocks);
-//     let bus = SeesawBus::new(i2c, delay);
-//     let mut encoder = NeoSlider::begin_default(bus).expect("Failed to connect
-// to neoslider");
-
-//     rprintln!(
-//         "{:#?}",
-//         encoder.capabilities().expect("Failed to get options")
-//     );
-
-//     loop {}
-// }
-
-// #[panic_handler]
-// fn handle_panic(info: &core::panic::PanicInfo) -> ! {
-//     rprintln!("PANIC! {}", info);
-//     rprintln!("Location {:?}", info.location());
-//     if let Some(pl) = info.payload().downcast_ref::<&str>() {
-//         rprintln!("Payload {:?}", pl);
-//     }
-//     loop {}
-// }
