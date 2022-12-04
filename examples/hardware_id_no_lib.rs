4e24238d25cbae08d8ee1962948ad8b5e8b42e7f
Alex Eden <alexandereden91@gmail.com>
2022-12-03 23:18:24 -0600

    Verify hardware id example

--- examples/hardware_id_no_lib.rs
+++ examples/hardware_id_no_lib.rs
@@ -2,28 +2,21 @@
 #![no_main]
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32f4xx_hal::{
-    gpio::GpioExt,
-    i2c::I2c,
-    pac,
-    prelude::*,
-    rcc::{RccExt, SYSCLK_MAX},
-};
+use stm32f4xx_hal::{gpio::GpioExt, i2c::I2c, pac, prelude::*, rcc::RccExt};
 
 const ADDR: u8 = 0x30;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Starting");
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
     let gpiob = dp.GPIOB.split();
-    let clocks = dp.RCC.constrain().cfgr.sysclk(SYSCLK_MAX.Hz()).freeze();
+    let clocks = dp.RCC.constrain().cfgr.freeze();
     let mut delay = cp.SYST.delay(&clocks);
     let scl = gpiob.pb6.into_alternate_open_drain::<4>();
     let sda = gpiob.pb7.into_alternate_open_drain::<4>();
-    let mut i2c = I2c::new(dp.I2C1, (scl, sda), 400.kHz(), &clocks);
+    let mut i2c = I2c::new(dp.I2C1, (scl, sda), 100.kHz(), &clocks);
 
     // Reset device
     i2c.write(ADDR, &[0x00, 0x7F, 0xFF])
@@ -37,7 +30,7 @@ fn main() -> ! {
         .and_then(|_| i2c.write_read(ADDR, &[], &mut id_buf))
         .expect("Failed to read hardware ID register");
 
-    rprintln!("Hardware ID {:?}", id_buf[0]);
+    rprintln!("Hardware ID {:x?}", id_buf[0]);
 
     loop {}
 }
