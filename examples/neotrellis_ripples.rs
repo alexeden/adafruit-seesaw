f15d7354464b8565a10584fc951673700b5ebc3a
Alex Eden <alexandereden91@gmail.com>
2025-01-30 13:27:19 -0600

    Improve the ripples example

--- examples/neotrellis_ripples.rs
+++ examples/neotrellis_ripples.rs
@@ -2,7 +2,7 @@
 #![no_main]
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
-use core::ops::{Add, AddAssign};
+use core::ops::{Add, AddAssign, Mul};
 
 /// https://github.com/adafruit/Adafruit_Seesaw/blob/master/examples/NeoTrellis/ripples/ripples.ino
 use adafruit_seesaw::{devices::NeoTrellis, prelude::*, SeesawRefCell};
@@ -10,10 +10,9 @@ use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32f4xx_hal::{gpio::GpioExt, i2c::I2c, pac, prelude::*, rcc::RccExt};
 
+const FALLOFF_R: f32 = 4.;
 const MAX_RIPPLES: usize = 16;
-const FALLOFF_TIME: usize = 64;
-const NUM_POINTS: usize = 8;
-const RIPPLE_RATE: f32 = 0.1;
+const RIPPLE_RATE: f32 = 0.2;
 const COLORS: [Color; 6] = [
     Color(255, 0, 0),
     Color(0, 255, 0),
@@ -29,12 +28,31 @@ struct Point {
     y: f32,
 }
 
+impl Point {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+
+    pub fn new_from_index(index: usize) -> Self {
+        Self {
+            x: (index % 4) as f32,
+            y: (index / 4) as f32,
+        }
+    }
+
+    pub fn cheby_dist(&self, other: &Self) -> f32 {
+        (self.x - other.x).abs().max((self.y - other.y).abs())
+    }
+}
+
 #[derive(Copy, Clone, Debug, Default)]
 struct Ripple {
     center: Point,
-    t: usize,
+    /// Radius from the ripple's center; incremented each loop by RIPPLE_RATE
+    /// When it reaches FALLOFF_R, the ripple is dropped
+    r: f32,
+    /// Color of the ripple
     c: Color,
-    points: [Point; NUM_POINTS],
 }
 
 #[derive(Copy, Clone, Debug, Default)]
@@ -52,6 +70,30 @@ impl Iterator for ColorWheel {
         Some(COLORS[self.0])
     }
 }
+
+// const POINTS: [Point; 16] = {
+//     let mut points = [Point::default(); 16];
+//     for i in 0..4 {
+//         for j in 0..4 {
+//             points[i * 4 + j] = Point::new(i as f32, j as f32);
+//         }
+//     }
+//     points
+// };
+
+//     Point::new(1.0, 0.0),
+//     Point::new(1.0, 1.0),
+//     Point::new(1.0, 2.0),
+//     Point::new(1.0, 3.0),
+//     Point::new(2.0, 0.0),
+//     Point::new(2.0, 1.0),
+//     Point::new(2.0, 2.0),
+//     Point::new(2.0, 3.0),
+//     Point::new(3.0, 0.0),
+//     Point::new(3.0, 1.0),
+//     Point::new(3.0, 2.0),
+//     Point::new(3.0, 3.0),
+// ];
 
 #[entry]
 fn main() -> ! {
@@ -76,7 +118,7 @@ fn main() -> ! {
         for y in 0..trellis.num_rows() {
             trellis
                 .set_key_event_triggers(x, y, &[KeyEventType::Pressed], true)
-                .and_then(|_| trellis.set_xy_neopixel_color(x, y, 0, 50, 0))
+                .and_then(|_| trellis.set_xy_neopixel_color(x, y, 10, 10, 10))
                 .expect("Failed to set key events and neopixel color");
         }
     }
@@ -90,11 +132,6 @@ fn main() -> ! {
     rprintln!("Looping...");
 
     loop {
-        // Clear the matrix
-        for c in matrix.iter_mut().map(|x| x.iter_mut()).flatten() {
-            *c = Color::default();
-        }
-
         // Process events
         for event in trellis.read_key_events().expect("Failed to read events") {
             match event.event {
@@ -102,17 +139,10 @@ fn main() -> ! {
                     for maybe_ripple in ripples.iter_mut() {
                         if let None = maybe_ripple {
                             let mut ripple = Ripple::default();
-                            ripple.center = Point {
-                                x: event.x as f32,
-                                y: event.y as f32,
-                            };
-                            ripple.t = 0;
+                            ripple.center = Point::new(event.x as f32, event.y as f32);
+                            ripple.r = 0.;
                             ripple.c = color_wheel.next().unwrap();
-                            rprintln!("Ripple color: {:?}", ripple.c);
-                            rprintln!("Ripple center: {:?}", ripple.center);
-                            ripple.points = [ripple.center; NUM_POINTS];
                             *maybe_ripple = Some(ripple);
-                            matrix[event.x as usize][event.y as usize] = ripple.c;
                             break;
                         }
                     }
@@ -121,62 +151,47 @@ fn main() -> ! {
             }
         }
 
-        let mut update = false;
+        for y in 0..trellis.num_cols() {
+            for x in 0..trellis.num_rows() {
+                matrix[y as usize][x as usize] = Color::default();
+                for maybe_ripple in ripples.iter() {
+                    if let Some(ripple) = maybe_ripple {
+                        let dist = ripple.center.cheby_dist(&Point::new(x as f32, y as f32));
+                        let z = 1. - (ripple.r - dist).abs();
+                        let color = ripple.c * z;
+                        matrix[y as usize][x as usize] += color;
+                    }
+                }
+            }
+        }
 
         for maybe_ripple in ripples.iter_mut() {
             if let Some(ripple) = maybe_ripple {
-                update = true;
-                ripple.points[0].x += RIPPLE_RATE; // / 2.;
-                ripple.points[1].x += RIPPLE_RATE; // / 2.;
-                ripple.points[1].y += RIPPLE_RATE; // / 2.;
-                ripple.points[2].y += RIPPLE_RATE;
-                ripple.points[3].x -= RIPPLE_RATE; // / 2.;
-                ripple.points[3].y += RIPPLE_RATE; // / 2.;
-                ripple.points[4].x -= RIPPLE_RATE;
-                ripple.points[5].x -= RIPPLE_RATE; // / 2.;
-                ripple.points[5].y -= RIPPLE_RATE; // / 2.;
-                ripple.points[6].y -= RIPPLE_RATE;
-                ripple.points[7].x += RIPPLE_RATE; // / 2.;
-                ripple.points[7].y -= RIPPLE_RATE; // / 2.;
+                ripple.r += RIPPLE_RATE;
 
-                for p in ripple.points.iter() {
-                    let x = p.x as i8;
-                    let y = p.y as i8;
-                    if x >= 0
-                        && x < trellis.num_cols() as i8
-                        && y >= 0
-                        && y < trellis.num_rows() as i8
-                    {
-                        matrix[x as usize][y as usize] += ripple.c;
-                    }
-                }
-
-                ripple.t += 1;
-
-                if ripple.t > FALLOFF_TIME {
+                if ripple.r > FALLOFF_R {
+                    rprintln!("Ripple falling off");
                     *maybe_ripple = None;
                 }
             }
         }
 
-        if update {
-            let mut next_colors = [(0, 0, 0); 16];
-            for (i, c) in matrix
-                .iter_mut()
-                .map(|x| x.iter_mut())
-                .flatten()
-                .enumerate()
-            {
-                next_colors[i] = (c.0, c.1, c.2);
-            }
-            // Update ripples
-            trellis
-                .set_neopixel_colors(&next_colors)
-                .and_then(|_| trellis.sync_neopixel())
-                .expect("Failed to set neopixel color");
-
-            trellis.sync_neopixel().expect("Failed to sync neopixel");
+        let mut next_colors = [(0, 0, 0); 16];
+        for (i, c) in matrix
+            .iter_mut()
+            .map(|x| x.iter_mut())
+            .flatten()
+            .enumerate()
+        {
+            next_colors[i] = (c.0, c.1, c.2);
         }
+        // Update ripples
+        trellis
+            .set_neopixel_colors(&next_colors)
+            .and_then(|_| trellis.sync_neopixel())
+            .expect("Failed to set neopixel color");
+
+        trellis.sync_neopixel().expect("Failed to sync neopixel");
     }
 }
 
@@ -212,6 +227,18 @@ impl Add for Color {
             self.0.saturating_add(rhs.0),
             self.1.saturating_add(rhs.1),
             self.2.saturating_add(rhs.2),
+        )
+    }
+}
+
+impl Mul<f32> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self(
+            (self.0 as f32 * rhs) as u8,
+            (self.1 as f32 * rhs) as u8,
+            (self.2 as f32 * rhs) as u8,
         )
     }
 }
