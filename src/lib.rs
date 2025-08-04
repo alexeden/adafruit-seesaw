d0b6e6ba19c899fd8f2c54496814196dd70b51d1
Ralph Ursprung <ralph.ursprung@gmail.com>
2025-08-04 10:48:07 +0200

    add `impl Error for SeesawError`

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![allow(const_evaluatable_unchecked, incomplete_features, rustdoc::bare_urls)]
 #![cfg_attr(feature = "module_neopixel", feature(generic_const_exprs))]
 
+use core::fmt::{Display, Formatter};
 // Re-export rgb
 pub use rgb;
 
@@ -37,4 +38,22 @@ pub enum SeesawError<E> {
     I2c(E),
     /// Occurs when an invalid hardware ID is read
     InvalidHardwareId(u8),
+}
+
+impl<E> Display for SeesawError<E> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        match self {
+            SeesawError::I2c(_) => f.write_str("I2C error"),
+            SeesawError::InvalidHardwareId(id) => write!(f, "invalid hardware id: {id}"),
+        }
+    }
+}
+
+impl<E: core::fmt::Debug + core::error::Error + 'static> core::error::Error for SeesawError<E> {
+    fn source(&self) -> Option<&(dyn core::error::Error + 'static)> {
+        match self {
+            SeesawError::I2c(err) => Some(err),
+            _ => None,
+        }
+    }
 }
